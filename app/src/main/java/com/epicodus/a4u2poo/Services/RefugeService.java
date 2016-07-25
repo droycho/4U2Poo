e519c3ae5336928c85cf7d55cd6dc5583e3ebe11
John Klein <johnmbklein@gmail.com>
2016-07-25 11:25:22 -0700

    Created basic RefugeService. Stored url in Constants.java

---
+++ app/src/main/java/com/epicodus/a4u2poo/Services/RefugeService.java
@@ -0,0 +1,31 @@
+package com.epicodus.a4u2poo.Services;
+
+import com.epicodus.a4u2poo.Constants;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Guest on 7/25/16.
+ */
+public class RefugeService {
+
+    public static void queryRefuge(float latitude, float longitude, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.REFUGE_BASE_URL).newBuilder();
+        String url = urlBuilder.build().toString();
+
+        Request request = Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+
+    }
+}
