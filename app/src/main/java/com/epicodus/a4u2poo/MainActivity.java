c60b54a910c2fb44639266d781a9d6c969e932c8
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-25 09:48:03 -0700

    Create Deatil and Login activities, add login and search buttons to main activity, buttons navigate to correct activities.

--- app/src/main/java/com/epicodus/a4u2poo/MainActivity.java
+++ app/src/main/java/com/epicodus/a4u2poo/MainActivity.java
@@ -1,13 +1,40 @@
 package com.epicodus.a4u2poo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.searchButton) Button mSearchButton;
+    @Bind(R.id.loginButton) Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+//        SearchButton from MainPage
+        if (view == mSearchButton) {
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            startActivity(intent);
+        }
+//        LoginButton from Mainpage
+        if (view == mLoginButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
+
