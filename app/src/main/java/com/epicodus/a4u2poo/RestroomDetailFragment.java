1fb25add62e6f88c1a6fe81727d1d153363ce55a
Ryan Carlile <ryancarlile@gmail.com>
2016-07-27 12:02:33 -0700

    Add RestroomListAdapter with recycler view

---
+++ app/src/main/java/com/epicodus/a4u2poo/RestroomDetailFragment.java
@@ -0,0 +1,29 @@
+package com.epicodus.a4u2poo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RestroomDetailFragment extends Fragment {
+
+//
+//    public RestroomDetailFragment() {
+//        // Required empty public constructor
+//    }
+//
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_toilet_detail, container, false);
+//    }
+
+}
