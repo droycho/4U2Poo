22cd6b04719682330abc28d61ecbfc3f25b4fd90
Ryan Carlile <ryancarlile@gmail.com>
2016-07-28 14:10:56 -0700

    Implement new google key and refactor restroomlistactivity to main activity

--- app/src/main/java/com/epicodus/a4u2poo/MainActivity.java
+++ app/src/main/java/com/epicodus/a4u2poo/MainActivity.java
@@ -1,22 +1,32 @@
 package com.epicodus.a4u2poo;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.epicodus.a4u2poo.Models.Restroom;
 import com.epicodus.a4u2poo.Services.RefugeService;
 import com.epicodus.a4u2poo.adapters.RestroomListAdapter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.parceler.Parcels;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,8 +36,17 @@ import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity implements
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, View.OnClickListener{
+
+
+        public static final String TAG = RestroomListActivity.class.getSimpleName();
     @Bind(R.id.searchButton) Button mSearchButton;
     @Bind(R.id.loginButton) Button mLoginButton;
     @Bind(R.id.bCreateAccount) Button mCreateAccount;
@@ -37,12 +56,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RestroomListAdapter mAdapter;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private double mLatitude;
+    private double mLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -60,12 +92,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSearchButton.setOnClickListener(this);
         mCreateAccount.setOnClickListener(this);
         mLoginButton.setOnClickListener(this);
-        getRestrooms("Portland");
+
+    }
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        MainActivityPermissionsDispatcher.requestLocationWithCheck(this);
+        getRestrooms(mLatitude, mLongitude, false, false);
     }
 
-    private void getRestrooms(String location) {
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (i == CAUSE_SERVICE_DISCONNECTED) {
+            Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+        } else if (i == CAUSE_NETWORK_LOST) {
+            Toast.makeText(this, "Network lost. Please re-connect.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "connectionResult: " + connectionResult.toString());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void getRestrooms(double lat, double lng, boolean ada, boolean unisex) {
         final RefugeService refugeService = new RefugeService();
-        refugeService.queryRefuge(45.5231, -122.6765,false, false, new Callback() {
+        refugeService.queryRefuge(lat, lng, ada, unisex, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
@@ -74,10 +150,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-//                if (response.isSuccessful()) {
+                if (response.isSuccessful()) {
                     mRestrooms = refugeService.processResults(response);
-//                        Log.d(TAG, mRestrooms.get(0).toString());
-
                     MainActivity.this.runOnUiThread(new Runnable() {
 
                         @Override
@@ -90,42 +164,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             mRecyclerView.setHasFixedSize(true);
                         }
                     });
-//                }
+                }
 
             }
         });
-
-
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
+    @NeedsPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    public void requestLocation() {
+        Log.d(TAG, "mGoogleApiClient.isConnected(): " + mGoogleApiClient.isConnected());
+        try {
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        } catch (SecurityException e) {
+            Log.e(TAG, "SecurityException:");
+            e.printStackTrace();
+        }
+        if (mCurrentLocation != null) {
+            Log.d(TAG, "Non-null location");
+            mLatitude = mCurrentLocation.getLatitude();
+            mLongitude = mCurrentLocation.getLongitude();
+            Log.d(TAG, mLatitude + ", " + mLongitude);
+        } else {
+            mLocationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(10 * 1000)
+                    .setFastestInterval(1 * 1000);
+            try {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            } catch (SecurityException e) {
+                Log.e(TAG, "SecurityException:");
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+//    @OnShowRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)
+//    void showRationaleForCamera(PermissionRequest request) {
+//        new AlertDialog.Builder(this)
+//                .setMessage("4U2Poo uses your location to find the nearest restrooms")
+//                .setPositiveButton(R.string.button_allow, (dialog, button) -> request.proceed())
+//                .setNegativeButton(R.string.button_deny, (dialog, button) -> request.cancel())
+//                .show();
+//    }
+
+    @OnPermissionDenied(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    void showDeniedForCamera() {
+        Toast.makeText(this, R.string.permission_location_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    void showNeverAskForCamera() {
+        Toast.makeText(this, R.string.permission_location_neverask, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View view) {
-//        SearchButton from MainPage
+
         if (view == mSearchButton) {
-            Intent intent = new Intent(MainActivity.this, RestroomListActivity.class);
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            intent.putExtra("restrooms", Parcels.wrap(mRestrooms));
             startActivity(intent);
         }
-//        LoginButton from Mainpage
         if (view == mLoginButton) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
@@ -135,6 +246,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
